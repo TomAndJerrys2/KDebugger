8881be69a01a6d33436d0eaea2bcf3e046aafe6d
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-19 18:19:21 +0000

    General layout for debugger

---
+++ include/libkdebugger/kdebugger.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace kdebugger {
+	
+	void init_debugger() const;
+}
---
+++ src/kdebugger.cpp
@@ -0,0 +1,9 @@
+// General includes
+#include <iostream>
+
+// Project include paths
+#include <libkdebugger/kdebugger.hpp>
+
+void kdebugger::init_debugger() const {
+	std::cout << "test" << '\n';
+}
