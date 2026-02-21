5e5e5ffb680e8f29845f3bd84efad81fc28d6369
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-21 15:01:51 +0000

    Basic functionality skeleton for attaching to PIDs

--- tools/kdebugger.cpp
+++ tools/kdebugger.cpp
@@ -1,9 +1,31 @@
+// General Headers
+#include <iostream>
+#include <unistd.h>
+
+// Private/Project-specific headers
 #include <libkdebugger/libkdebugger.hpp>
 
-// Main Execution
-int main() {
+namespace {
 	
-	kdebugger::init_debugger();
+	// attachs to a currently running process ID
+	// based on the command-line argument passed
+	const pid_t attach(int argc, const char** argv);
+};
+
+// Main Execution
+int main(int argc, const char** argv) {
+	
+	// if our arguments are one
+	// i.e a -p is passed to specify a
+	// Linux process ID
+	if(argc == 1) {
+		std::cerr << "> No arguments were passed\n";
+		return -1;
+	}	
+	
+	// attach to the current PID passed
+	// and return it to this variable
+	pid_t pid = attach(argc, argv);
 
 	return 0;
 }
