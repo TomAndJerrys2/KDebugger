fb334e50b086e8e42296e3667d754f32887022d1
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-22 13:18:05 +0000

    Close Read/Write && Release Read/Write buffer

--- src/pipe.cpp
+++ src/pipe.cpp
@@ -1,6 +1,7 @@
 // General Headers
 #include <unistd.h>
 #include <fcntl.h>
+#include <utility>
 
 // Private / Project-specific headers
 #include <libkdebugger/pipe.hpp>
@@ -13,4 +14,33 @@ kdebugger::pipe::pipe(bool close_on_exec) {
 	}
 }
 
+// release the read buffer, exchanging for -1
+constexpr int kdebugger::pipe::release_read() noexcept {
+	return std::exchange(m_Fds[read_fd], -1);
+}
 
+// release the write buffer, exchanging for -1
+constexpr int kdebugger::pipe::release_write() noexcept {
+	return std::exchange(m_Fds[write_fd], -1);
+}
+
+void kdebugger::pipe::close_read() const {
+	if(m_Fds[read_fd] != -1) {
+		close(m_Fds[read_fd]);
+		m_Fds[read_fd] = -1;
+	}
+}
+
+void kdebugger::pipe::close_write() const {
+	if(m_Fds[write_fd] != -1) {
+		close(m_Fds[write_fd]);
+		m_Fds[write_fd] = -1;
+	}
+}
+
+// destructor handles closing the read and write buffers
+// stored as indexes in m_Fd
+kdebugger::pipe::~pipe() {
+	close_read();
+	close_write();
+}
