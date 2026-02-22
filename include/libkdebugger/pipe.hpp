e2c882f7b24fe7d9938cce782fe0a90f5d4eb744
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-22 11:23:16 +0000

    Outlined a pipe class for child and parent interprocess communication

---
+++ include/libkdebugger/pipe.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <vector>
+#include <cstddef>
+
+namespace kdebugger {
+	
+	// pipe class for Parent-child process communication
+	class pipe {
+		
+		public:
+			explicit pipe(bool close_on_exec);
+			~pipe();
+
+			constexpr int get_read() const {
+				return m_Fds[read_fd];
+			}
+
+			constexpr int get_write() const {
+				return m_Fds[write_fd];
+			}
+
+			// read and write from 64K buffer
+			std::vector<std::byte> read();
+			void write(std::byte * from, const std::size_t bytes);
+
+		private:
+			int m_Fds[2];
+			static constexpr unsigned int read_fd {0};
+		       	static constexpr unsigned int write_fd {0};	
+	};
+}
