21112c12690bab59bf2f9b7cf30375c349168f94
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-22 11:02:07 +0000

    Working on automated testing

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,14 @@
 #include <catch2/catch_test_macros.hpp>
+#include <libkdebugger/process.hpp>
 
-TEST_CASE("validate environment") {
-	REQUIRE(false);
+using namespace kdebugger;
+
+namespace {
+	
+	bool process_exists(const pid_t pid);
+}
+
+TEST_CASE("process::launch success", "[process]") {
+	auto proc = process::launch("yes");
+	REQUIRE(process_exists(proc->pid));
 }
