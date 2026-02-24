a6e421b88478c0ca3398f424b63e2899e91133ad
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-24 18:27:33 +0000

    Defined basic class for a register and its operations of reading/writing information about a process to it and surrounding meta-data

---
+++ include/libkdebugger/registers.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace kdebugger {
+
+	class process;
+	class registers {
+	
+		private:
+			
+			friend process;
+			user m_Data;
+			process * m_Process
+			
+			registers(process & proc) : m_Process {proc} {} 
+
+		public:
+			// delete default constructor
+			registers() = delete;
+
+			// delete copy constructor && copy assignment
+			registers(const registers &) = delete;
+			registers & operator=(const registers &) = delete;
+			
+			using value = //...;
+			value read(const register_info & info) const;
+			void write(const register_info & info, value val);
+	};
+}
