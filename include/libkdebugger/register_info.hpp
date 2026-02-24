4deb11f6a408d686a95c83089a622d68424e88d5
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-24 18:18:44 +0000

    Added helper functions that return a callback thats passed into register_info_by to gather meta-data about a given register

--- include/libkdebugger/register_info.hpp
+++ include/libkdebugger/register_info.hpp
@@ -6,7 +6,7 @@
 #include <sys/user.h>
 
 namespace kdebugger {
-	// ID of a given 64bit register
+	// ID of a given 64bit register -> detail/registers.inc
 	enum class register_id {
 		#define DEFINE_REGISTER(name, dwarf_id, size, offset, size, format) name
 		#include <libkdebugger/detail/registers.inc>
@@ -34,11 +34,40 @@ namespace kdebugger {
 		register_type type;
 		register_format fmt;
 	};
-
+	
+	// information about a given type of register -> detail/registers.inc
 	inline constexpr const register_info g_register_infos[] = {
 		#define DEFINE_REGISTER (name, dwarf_id, size, offset, type, format) \
 			{ register_id::name, #name, dwarf_id, size, offset, type, format}
 		#include <libkdebugger/detail/registers.inc>
 		#undef
 	};
+
+	template<class F> const register_info & register_info_by(F f) {
+		auto it = std::find_if(
+				std::begin(g_register_infos), 
+				std::end(g_register_infos),
+				f				
+		);
+
+		if(it == std::end(g_register_infos))
+			error::send("Cannot find register info");
+
+		return *it;
+	}	
+	
+	// get a register by its id
+	inline const register_info & register_by_id(register_id id) {
+		return register_info_by([id] (auto & i) {return i.id == id; });
+	}
+
+	// get a register by its name
+	inline const register_info & register_info_by_name(std::string_view name) {
+		return register_info_by([name] (auto & i) { return i.name == name; });
+	}
+
+	// get a register by its DWARF id
+	inline const register_info & register_info_by_dwarf(std::int32_t dwarf_id) {
+		return register_info_by([dwarf_id] (auto & i) { return i.dwarf_id == dwarf_id; });
+	}
 }
