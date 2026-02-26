68ab31461d41b656f953a59c713ac0a34dbb45e0
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-26 18:56:20 +0000

    Trap + kill syscalls

--- test/targets/reg_write.s
+++ test/targets/reg_write.s
@@ -7,7 +7,18 @@
 main:
 	push %rbp
 	movq %rsp, %rbp
-	
+
+	# Get PID
+	movq $39, %rax
+	syscall
+	movq %rax, %r12
+
+	# Trap
+	movq $62, %rax
+	movq %r12, %rdi
+	movq $5, %rsi
+	syscall	
+
 	popq %rbp
 	movq $0, %rax
 
