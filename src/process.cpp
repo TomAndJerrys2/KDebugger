b8f0aa258f2466a3df0f81b4c7d011c8a5c51219
TomAndJerrys2 <kylev2005@hotmail.com>
2026-03-03 18:18:02 +0000

    Added a read memory helper function to ignore trap calls and represent int3 calls in there original byte instruction format

--- src/process.cpp
+++ src/process.cpp
@@ -278,6 +278,21 @@ std::vector<std::byte> kdebugger::process::read_memory(virt_addr address, std::s
 	return ret;
 }
 
+std::vector<std::byte> kdebugger::process::read_memory_without_traps(virt_addr address, std::size_t amount ) const {
+	auto memory = read_memory(address, amount);
+	auto sites = m_BreakPointSites.get_in_region(address, address + amount);
+
+	for(auto site : sites) {
+		if(!site->is_enabled())
+			continue;
+
+		auto offset = site->address() - address.addr();
+		memory[offset.addr()] = site->m_SavedData;
+	}
+
+	return memory;
+}
+
 void kdebugger::process::write_memory(virt_addr address, span<const std::byte> data) {
 	std::size_t written {0};
 
