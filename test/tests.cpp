b62cfac001cb41b8f767d066bf165069a46e8513
TomAndJerrys2 <kylev2005@hotmail.com>
2026-03-08 11:16:01 +0000

    Added test case to check if catchpoints halt program execution

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <elf.h>
 #include <regex>
+#include <fcntl.h>
 
 #include <catch2/catch_test_macros.hpp>
 #include <libkdebugger/process.hpp>
@@ -503,4 +504,34 @@ TEST_CASE("Syscall mapping works", "[syscall]") {
 	REQUIRE(kdebugger::syscall_name_to_id("read") == 0);
 	REQUIRE(kdebugger::syscall_id_to_name(62) == "kill");
 	REQUIRE(kdebugger::syscall_name_to_id("kill") == 62);
+}
+
+// in case - ensuring a process halts for catchpoints which are set
+TEST_CASE("Syscall catchpoints work", "[catchpoint]") {
+	auto dev_null = open("/dev/null", O_WRONGLY);
+	auto proc = process::launch("targets/anti_debugger", true, dev_null);
+
+	auto write_syscall = kdebugger::syscall_name_to_id("write");
+	auto policy = kdebugger::syscall_catch_policy::catch_some({write_syscall});
+	proc->set_syscall_catch_policy(policy);
+
+	proc->resume();
+	auto reason = proc->wait_on_signal();
+
+	REQUIRE(reason.reason == kdebugger::process_state::stopped);
+	REQUIRE(reason.info == SIGTRAP);
+	REQUIRE(reason.trap_reason == kdebugger:trap_type::syscall);
+	REQUIRE(reason.syscall_info->id == write_syscall);
+	REQUIRE(reason.syscall_info->entry == true);
+
+	proc->resume();
+	reason = proc->wait_on_signal();
+
+	REQUIRE(reason.reason == kdebugger::process_state::stopped);
+	REQUIRE(reason.info == SIGTRAP);
+	REQUIRE(reason.trap_reason == kdebugger::trap_type::syscall);
+	REQUIRE(reason.syscall_info->id == write_syscall);
+	REQUIRE(reason.syscall_info->entry == true);
+
+	close(dev_null);
 }
