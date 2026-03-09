b78a3e5f8ba7693f90d9f037d51af8aa3a400e69
TomAndJerrys2 <kylev2005@hotmail.com>
2026-03-09 17:18:59 +0000

    Added helper functions in types.cpp for converting to and from virtual address' and file address'

---
+++ src/types.cpp
@@ -0,0 +1,24 @@
+// Private / Project specific headers
+#include <libkdebugger/types.hpp>
+#include <libkdebugger/elf.hpp>
+
+// general include paths
+#include <cassert>
+
+kdebugger::virt_addr kdebugger::file_addr::to_virt_addr() const {
+	assert(m_Elf && "to_virt_addr called on null address");
+
+	auto section = m_Elf->get_section_containing_address(*this);
+	if(!section)
+		return virt_addr{};
+
+	return virt_addr { m_Addr + m_Elf->load_bias().addr() };
+}
+
+kdebugger::file_addr kdebugger::virt_addr::to_file_addr(const elf & obj) const {
+	auto section = obj.get_section_containing_address(*this);
+	if(!section)
+		return file_addr{};
+
+	return file_addr { obj, m_Addr - obj.load_bias().addr() };
+}
