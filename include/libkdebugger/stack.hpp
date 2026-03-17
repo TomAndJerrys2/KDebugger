620cca6f4bf91389376729b3e8cc4a0397906550
TomAndJerrys2 <kylev2005@hotmail.com>
2026-03-17 17:34:33 +0000

    Added stack class for future representations of stack frames for the inline stack

---
+++ include/libkdebugger/stack.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+// general includes
+#include <vector>
+
+// Private / project specific includes
+#include <libkdebugger/dwarf.hpp>
+
+namespace kdebugger {
+	class target;
+
+	class stack {
+		
+		private:
+			target * m_Target = nullptr;
+			std::uint32_t m_InlineHeight {0};
+
+		public:
+			stack(target * tgt) : m_Target {tgt} {}
+			void reset_inline_height();
+			std::vector<kdebugger::die> inline_stack_at_pc() const;
+			
+			std::uint32_t inline_height() {
+				return m_InlineHeight;
+			}
+
+			const target & get_target() const {
+				return *m_Target;
+			}
+	}
+}
