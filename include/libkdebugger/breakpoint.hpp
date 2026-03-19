709c60cd1d8fc4a056c6adc42e6ee774a57e0c9c
TomAndJerrys2 <kylev2005@hotmail.com>
2026-03-19 19:25:52 +0000

    Implemented the breakpoint header

---
+++ include/libkdebugger/breakpoint.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+// General Includes
+#include <cstdint>
+#include <cstddef>
+#include <string>
+
+// Private / project specific includes
+#include <libkdebugger/stoppoint_collection.hpp>
+#include <libkdebugger/breakpoint_site.hpp>
+#include <libkdebugger/types.hpp>
+
+namespace kdebugger {
+	class target;
+
+	class breakpoint {
+		
+		protected:
+			friend target;
+
+			breakpoint(target & tgt, bool is_hardware = false, bool is_internal = false);
+			
+			id_type m_Id;
+			target * m_Target;
+			bool m_IsEnabled {false};
+			bool m_IsHardware {false};
+			bool m_IsInternal {false};
+
+			breakpoint_site::id_type m_NextSiteId = 1;
+
+		public:
+			virtual ~breakpoint() = default;
+
+			breakpoint() = delete;
+			breakpoint(const breakpoint &) = delete;
+			breakpoint & operator = (const breakpoint &) = delete;
+
+			using id_type = std::int32_t;
+			id_type id() const {
+				return m_Id;
+			}
+
+			void enable();
+			void disable();
+
+			bool is_enabled() const {
+				return m_IsEnabled;
+			}
+
+			bool is_hardware() const {
+				return m_IsHardware;
+			}
+
+			bool is_internal() const {
+				return m_IsInternal;
+			}
+
+			virtual void resolve() const = 0;
+
+			// breakpoint_sites() { return m_BreakPointSites; }
+	};
+}
+
