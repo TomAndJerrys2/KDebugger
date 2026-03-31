a818e96a5f5a88bd4c9bda1f46d3781994a3c74d
TomAndJerrys2 <kylev2005@hotmail.com>
2026-03-31 18:07:44 +0100

    Added new test files for eventually demonstrating tracing dynamic/static libraries and ASLR

---
+++ test/targets/libmarshmellow.cpp
@@ -0,0 +1,5 @@
+extern int libkdebugger_client;
+
+bool libkdebugger_client_name() {
+    return libkdebugger_client > 50;
+}
---
+++ test/targets/marshmellow.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int libkdebugger_client = 100;
+bool libkdebugger_client_name();
+
+int main() {
+    std::cout << "Client number: " << libkdebugger_client << '\n';
+    std::cout << "Client name: " << libkdebugger_client_name();
+}
